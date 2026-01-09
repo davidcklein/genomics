a215e7c06cfd9d2ed1009481eb721ce7243a932a
davidcklein <131034346+davidcklein@users.noreply.github.com>
2026-01-08 20:37:44 -0500

    Update RNAseq_analysis.bash
    Updated modules

--- transcriptomics_analysis/RNAseq_analysis.bash
+++ transcriptomics_analysis/RNAseq_analysis.bash
@@ -8,24 +8,24 @@
 #SBATCH -p scavenger
 ##################################
 
-module load gcc/8.2.0 star/2.7.0e samtools/1.14
+module load gcc star/2.7.0e samtools
 
 hg38=/ix1/shainer/Dave/Master_Reference_Files/programs/STAR/hg38/
 mm10=/ix1/shainer/Dave/Master_Reference_Files/programs/STAR/mm10/mm10_gencode
 
-for f in *R1.fastq; do STAR --runThreadN 4 --genomeDir $hg38 --readFilesIn $f ${f/R1/R2} --outSAMtype BAM SortedByCoordinate --outFilterMismatchNoverReadLmax 0.02 --outFilterMultimapNmax 1 --outFileNamePrefix ${f/_R1.fastq/}; done
+for f in *R1.fastq; do STAR --runThreadN 4 --genomeDir $T2T --readFilesIn $f ${f/R1/R2} --outSAMtype BAM SortedByCoordinate --outFilterMismatchNoverReadLmax 0.02 --outFilterMultimapNmax 1 --outFileNamePrefix ${f/_R1.fastq/}; done
 #Align read fragments using a splice-aware aligner (I prefer STAR, but Hisat2 is another good option. I would avoid TopHat altogether at this point). 
 
 for f in *.bam; do mv $f ${f/Aligned.sortedByCoord.out/}; done
 
 module purge
-module load gcc/8.2.0 samtools/1.14
+module load gcc samtools
 
 for f in *.bam; do samtools view -@ 3 -h -O BAM -q 7 -o ${f/.bam/_qf.bam} $f; samtools sort -@ 3 -o ${f/.bam/_sorted.bam} ${f/.bam/_qf.bam}; samtools index -@ 3 ${f/.bam/_sorted.bam}; done
 #This command will filter out reads with a MAPQ alignment quality score of < 7, as well as coordinate-sort and index the resulting bam files
 
 module purge
-module load subread/2.0.1
+module load subread
 
 featureCounts -t transcript -s 2 -p -B -T 4 -a /ix1/shainer/Dave/Master_Reference_Files/hg38/gtf/hg38.gencode.v38.gtf -o Gencode_hg38_transcript_counts.txt *_sorted.bam
 featureCounts -t gene -s 2 -p -B -T 4 -a /ix1/shainer/Dave/Master_Reference_Files/hg38/gtf/hg38.gencode.v38.gtf -g gene_name -o Gencode_hg38_gene_counts.txt *_sorted.bam
